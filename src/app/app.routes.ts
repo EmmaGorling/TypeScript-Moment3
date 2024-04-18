a6804e7b0e36a475a6d1328fd43cb9e62f4202bc
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-04-18 13:58:58 +0200

    Gjort en 404-sida och fixat activelink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ConverterComponent } from './converter/converter.component';
 import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'converter', component: ConverterComponent},
-    {path: 'about', component: AboutComponent}
+    {path: 'about', component: AboutComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '404', component: NotFoundComponent},
+    {path: '**', component: NotFoundComponent}
 ];
