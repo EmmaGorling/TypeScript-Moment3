cfd13f7f02c5ade27cdf69a504f460aecc27b0f0
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-04-18 10:31:27 +0200

    Lagt till i om-sidan

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -11,34 +11,6 @@ import { CelsiusComponent } from '../celsius/celsius.component';
   styleUrl: './converter.component.scss'
 })
 export class ConverterComponent {
-  // Properties for meter too foot
-  meter: number = 0;
-  foot:number = 0;
-  celsius:number = 0;
-  fahrenheit:number = 0;
-  result:string = '';
-
-  /* Methods */
-
-  // From meter to foot
-  convertMeter():void {
-    this.result = (this.meter * 0.3048).toFixed(2);
-    this.foot = parseFloat(this.result);
-  }
-  // From foot to meter
-  convertFoot():void {
-    this.result = (this.foot * 3.2808399).toFixed(2);
-    this.meter = parseFloat(this.result);
-  }
-
-  convertCelsius():void {
-    this.result = ((this.celsius * 1.8) + 32).toFixed(2);
-    this.fahrenheit = parseFloat(this.result);
-  }
-
-  convertFahrenheit():void {
-    this.result = ((this.fahrenheit - 32) / 1.8).toFixed(2);
-    this.celsius = parseFloat(this.result);
-  }
+  
 
 }
