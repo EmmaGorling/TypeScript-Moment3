c81504ef79a98da3d259c17b652eae8c02f67b86
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-04-24 10:48:40 +0200

    Ändrat i routes för att se om routingproblemet löser sig

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,10 @@ import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'converter', component: ConverterComponent},
     {path: 'about', component: AboutComponent},
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: '404', component: NotFoundComponent},
     {path: '**', component: NotFoundComponent}
 ];
